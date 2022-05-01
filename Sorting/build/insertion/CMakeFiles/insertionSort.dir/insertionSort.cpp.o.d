9aaac78a1b59214cca1989ced7874f056834b734
Vahid Lari <vahid.lari@gmail.com>
2022-05-01 22:28:41 +0200

    removed the build folder

--- Sorting/build/insertion/CMakeFiles/insertionSort.dir/insertionSort.cpp.o.d
+++
@@ -1,3 +0,0 @@
-insertion/CMakeFiles/insertionSort.dir/insertionSort.cpp.o: \
-  /Users/vahid/MyProjects/DataStructuresAndAlgorithms/Sorting/insertion/insertionSort.cpp \
-  /Users/vahid/MyProjects/DataStructuresAndAlgorithms/Sorting/insertion/insertionSort.hpp
