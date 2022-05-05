a5641aea5dbddbfce2bb2e71e18a2d2d379b2d0d
Vahid Lari <vahid.lari@gmail.com>
2022-05-05 19:30:21 +0200

    improvements in the class inheritence, make file, etc

--- test/test.cpp
+++ test/test.cpp
@@ -2,34 +2,45 @@
 #include "insertion/insertionSort.hpp"
 #include "shell/shellSort.hpp"
 #include "merge/mergeSort.hpp"
+#include "quick/quickSort.hpp"
 #include "printArray.hpp" 
+
 
 int main()
 {
+    using complexity_t = CQuickSort::EImplementationComplexity;
     std::cout << "Hello world!\n";
     CSelectionSort<int> mySelectionSort;
     CInsertionSort<int> myInsertionSort;
     CShellSort<int> myShellSort;
     CMergeSort myMergeSort;
-    
+    CQuickSort mySimpleQuickSort(complexity_t::simple);
+    CQuickSort myEfficientQuickSort(complexity_t::efficient);
+
     int a[10] = {8, 9, 2, 4, 1, 5, 3, 0, 6, 7};
-    int b[10], c[10], d[10];
+    int b[10], c[10], d[10], e[10], f[10];
     for (int i  = 0; i < 10; i++){
-        b[i] = c[i] = d[i] = a[i];
+        b[i] = c[i] = d[i] = e[i] = f[i] = a[i];
     }
     printArray(a, 10);
 
     cout << "----- SELECTION SORT ------\n";
-    mySelectionSort.sort(a, 10, true);
+    mySelectionSort.sort(a, 10);
     printArray(a, 10);
 
     cout << "----- INSERTION SORT ------\n";
-    myInsertionSort.sort(b, 10, true);
+    myInsertionSort.sort(b, 10);
     printArray(b, 10);
     cout << "----- SHELL SORT ------\n";
-    myShellSort.sort(c, 10, true);
+    myShellSort.sort(c, 10);
     printArray(c, 10);
     cout << "----- MERGE SORT ------\n";
-    myMergeSort.sort(d, 10, true);
+    myMergeSort.sort(d, 10);
     printArray(d, 10);
+    cout << "----- QUICK SORT SIMPLE ------\n";
+    mySimpleQuickSort.sort(e, 10);
+    printArray(e, 10);
+    cout << "----- QUICK SORT EFFICIENT ------\n";
+    myEfficientQuickSort.sort(f, 10);
+    printArray(f, 10);
 }
