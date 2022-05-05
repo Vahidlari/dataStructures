04bf83cf3f30a67c7369ef3911e132283fe5244e
Vahid Lari <vahid.lari@gmail.com>
2022-05-05 09:50:01 +0200

    implemented a base class for sorting and derived merge sort on top of that

---
+++ test/baseSort.hpp
@@ -0,0 +1,43 @@
+#ifndef BASESORT_INCLUDED_
+#define BASESORT_INCLUDED_
+#include <iostream>
+
+using namespace std;
+
+
+class CBaseSort{
+public:
+    CBaseSort() 
+        : swapCount(0)
+    {
+    }
+
+    virtual void sort(int inArray[], int arraySize, bool print) = 0;
+    static void printArray(int a[], int size);
+    static void printArray(int a[], int low, int high);
+
+protected:
+    void swap(int inArray[], int x, int y);
+    int swapCount;
+};
+
+void CBaseSort::swap(int inArray[], int x, int y){
+    int tmp = inArray[x];
+    inArray[x] = inArray[y];
+    inArray[y] = tmp;
+    ++swapCount;
+}
+
+void CBaseSort::printArray(int a[], int size){
+    printArray(a, 0, size-1);
+}
+
+void CBaseSort::printArray(int a[], int low, int high){
+    for(int i = low; i <= high; i++){
+        cout << a[i] << ", ";
+    }
+    cout << std::endl;    
+}
+
+#endif //BASESORT_INCLUDED_
+
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include "selection/selectionSort.hpp"
 #include "insertion/insertionSort.hpp"
 #include "shell/shellSort.hpp"
+#include "merge/mergeSort.hpp"
 #include "printArray.hpp" 
 
 int main()
@@ -9,10 +10,13 @@ int main()
     CSelectionSort<int> mySelectionSort;
     CInsertionSort<int> myInsertionSort;
     CShellSort<int> myShellSort;
+    CMergeSort myMergeSort;
     
     int a[10] = {8, 9, 2, 4, 1, 5, 3, 0, 6, 7};
-    int b[10] = {8, 9, 2, 4, 1, 5, 3, 0, 6, 7};
-    int c[10] = {8, 9, 2, 4, 1, 5, 3, 0, 6, 7};
+    int b[10], c[10], d[10];
+    for (int i  = 0; i < 10; i++){
+        b[i] = c[i] = d[i] = a[i];
+    }
     printArray(a, 10);
 
     cout << "----- SELECTION SORT ------\n";
@@ -25,4 +29,7 @@ int main()
     cout << "----- SHELL SORT ------\n";
     myShellSort.sort(c, 10, true);
     printArray(c, 10);
+    cout << "----- MERGE SORT ------\n";
+    myMergeSort.sort(d, 10, true);
+    printArray(d, 10);
 }
