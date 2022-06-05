a4f18dd1773d5a971b4c40b165c84e0fb02642f9
Vahid Lari <vahid.lari@gmail.com>
2022-06-05 16:17:28 +0200

    Added cycle detection to undirected graphs

--- graphs/stronglyConnectedComponents/inc/depthFirstOrder.hpp
+++ graphs/stronglyConnectedComponents/inc/depthFirstOrder.hpp
@@ -15,13 +15,20 @@ public:
 
     CDepthFirstOrder() = default;
     stack_t postOrderList();
+    /// Provides a list of reverse post order of the visited nodes during a DFS. 
+    /// Basically, the reverse post order list represent the topological order of a 
+    /// Directed Acyclic Graph (DAG). Note, a topological order can be derived only and if only 
+    /// a graph is a DAG.
+    /// Reverse post order vetices are derived once a node is fully visited, including its edges.
     stack_t postOrderList(CDirectedGraph& graph);
 
 private:
     void dfs(CDirectedGraph& graph, vertix_index_t src);
 
     vertix_list_t visitedList;
+    vertix_list_t vertixRecursionStack;
     stack_t postOrder;
+    bool isGraphCyclic;
 };
 
 #endif //DEPTHFIRSTORDER_INCLUDED_
--- graphs/stronglyConnectedComponents/src/depthFirstOrder.cpp
+++ graphs/stronglyConnectedComponents/src/depthFirstOrder.cpp
@@ -17,14 +17,21 @@ typename CDepthFirstOrder::stack_t CDepthFirstOrder::postOrderList(CDirectedGrap
 
 void CDepthFirstOrder::dfs(CDirectedGraph& graph, vertix_index_t src){
     visitedList.insert(src);
+    vertixRecursionStack.insert(src);
     std::cout << std::endl << "Added " << src << " to the visited List and running dfs: ";
     for(auto& v: graph.getAdjacents(src))
     {
-        if(!visitedList.contains(v))
+        if(!visitedList.contains(v) && !isGraphCyclic)
         {
             dfs(graph, v);
+        } else if(vertixRecursionStack.contains(v))
+        {
+            isGraphCyclic = true;
+            std::cout << "Graph is cyclic! Node " << v << " is in the recursion stack!\n";
+            return;
         }
     }
+    vertixRecursionStack.erase(src);
     postOrder.push_front(src);
     std::cout << "Pushed " << src << " to the post order List\n";
 }
