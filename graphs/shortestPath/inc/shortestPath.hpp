74f6a9cd2271181c9aeb90545a77568059c8c676
Vahid Lari <vahid.lari@gmail.com>
2022-06-11 12:02:44 +0200

    minor commenting

--- graphs/shortestPath/inc/shortestPath.hpp
+++ graphs/shortestPath/inc/shortestPath.hpp
@@ -39,8 +39,8 @@ private:
     /// The algorithm maintains an array of distances from source to each vertix
     /// and an array indicating which input edge to a vertix contains in the shortest path.
     /// Starting from the source vertix, the algorithm updates a sorted list of distances to 
-    /// unvisited vertices. The list get updated when a vertix is visited, in which, the distances to 
-    /// its destination vertices are updated if they could be reached in a shorter patch compared to
+    /// unvisited vertices. The list gets updated when a vertix is visited, in which, the distances to 
+    /// its destination vertices are updated if they could be reached in a shorter path compared to
     /// existing values in the distanceTo list. Each time a distance to a vertix is updated, the sorted
     /// list of distance to unvisited vertices gets updated. 
     /// the iterations continue till there is no item in vertix distance sorted list.
